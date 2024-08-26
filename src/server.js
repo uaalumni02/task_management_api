3f8c8fd8b52a59c6feea4b6fe11625b4f4ed04e4
uaalumni02 <dbell1911@gmail.com>
2024-08-26 18:58:32 -0500

    connect db

--- src/server.js
+++ src/server.js
@@ -14,20 +14,14 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// const DB_URL = process.env.MONGO_URL;
-// const TEST_DB_URL = process.env.MONGO_TEST_URL;
+const DB_URL = process.env.MONGO_URL;
 
-// if (process.env.NODE_ENV == "test") {
-//   mongoose.connect(TEST_DB_URL, { useNewUrlParser: true }, (err) => {
-//     if (err) return log("Unable to Connect to MongoDB");
-//     return log("Connection Successful to test DB");
-//   });
-// } else {
-//   mongoose.connect(DB_URL, { useNewUrlParser: true }, (err) => {
-//     if (err) return log("Unable to Connect to MongoDB");
-//     return log("Connection Successful");
-//   });
-// }
+try {
+  mongoose.connect(DB_URL, { useNewUrlParser: true });
+  console.log("Connection Successful");
+} catch (err) {
+  console.error("Unable to Connect to MongoDB", err);
+}
 
 app.use("/api", router);
 
