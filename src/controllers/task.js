ca2a5eb0582648dfc3e285745aa9d856c597b9f9
uaalumni02 <dbell1911@gmail.com>
2024-09-23 18:45:40 -0500

    start task controller

---
+++ src/controllers/task.js
@@ -0,0 +1,28 @@
+import Db from "../db/db";
+import Task from "../models/task";
+
+import validator from "../validator/task";
+import * as Response from "../helpers/response/response";
+
+class TaskData {
+  static async addTask(req, res) {
+    const taskData = { ...req.body };
+
+    let taskTimestamp = moment(taskData.dueDate, "MM-DD-YYYY").unix();
+    taskData.dueDate = taskTimestamp;
+
+    try {
+      const result = await validator.validateAsync(taskData);
+      if (!result.error) {
+        const TaskName = await Db.addTask(Task, taskData);
+        return Response.responseOkCreated(res, TaskName);
+      }
+    } catch (error) {
+      return Response.responseServerError(res);
+    }
+  }
+}
+
+export default TaskData;
+
+// complete remainder of files for this controller....add .populate-------------------
